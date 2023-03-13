535dc472fe1c803fa4bbf3dfce12180451392496
KS LIM <potatoscript@yahoo.com>
2023-03-13 17:38:47 +0900

    added Controller

---
+++ api/Controllers/EmployeeController.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using JobApi.Models;
+using JobApi.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JobApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EmployeeController : ControllerBase
+    {
+        public EmployeeServices _employeeservice;
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="employeeservice">従業員Services</param>
+        public EmployeeController(EmployeeServices employeeservice)
+        {
+            _employeeservice = employeeservice;
+        }
+
+        /// <summary>
+        /// 従業員新規作成HTTPクエリ
+        /// </summary>
+        /// <param name="employee">従業員Model</param>
+        /// <returns></returns>
+        [HttpPost("create-employee")]
+        public IActionResult CreateEmployee(Employee employee)
+        {
+            _employeeservice.CreateEmployee(employee);
+            return Ok();
+        }
+
+        /// <summary>
+        /// 全従業員データ取得するHTTPクエリ
+        /// </summary>
+        /// <returns>全従業員データ</returns>
+        [HttpGet("read-all-employees")]
+        public IActionResult ReadAllEmployees()
+        {
+            var allEmployees = _employeeservice.ReadAllEmployees();
+            return Ok(allEmployees);
+        }
+
+        /// <summary>
+        /// 対象従業員データを取得するHTTPクエリ
+        /// </summary>
+        /// <param name="name">従業員名</param>
+        /// <returns></returns>
+        [HttpGet("read-employee/{name}")]
+        public IActionResult ReadEmployeeByName(string name)
+        {
+            var employee = _employeeservice.ReadEmployeeByName(name);
+            return Ok(employee);
+        }
+
+        /// <summary>
+        /// 対象従業員データを更新するHTTPクエリ
+        /// </summary>
+        /// <param name="employee">従業員Model</param>
+        /// <returns></returns>
+        [HttpPut("update-employee-by-id")]
+        public IActionResult UpdateEmployeeById(Employee employee)
+        {
+            var updateEmployee = _employeeservice.UpdateEmployeeById(employee);
+            return Ok(updateEmployee);
+        }
+
+        /// <summary>
+        /// 対象従業員データを削除するHTTPクエリ
+        /// </summary>
+        /// <param name="id">対象従業員ID</param>
+        /// <returns></returns>
+        [HttpDelete("delete-employee-by-id/{id}")]
+        public IActionResult DeleteEmployeeById(int id)
+        {
+            _employeeservice.DeleteEmployeeById(id);
+            return Ok();
+        }
+
+    }
+}
---
+++ api/Controllers/JobController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using JobApi.Models;
+using JobApi.Services;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace JobApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class JobController : ControllerBase
+    {
+        public JobServices _jobservice;
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="jobservice">業務Services</param>
+        public JobController(JobServices jobservice)
+        {
+            _jobservice = jobservice;
+        }
+
+        /// <summary>
+        /// 新規業務データを作成するHTTPクエリ
+        /// </summary>
+        /// <param name="job">業務Model</param>
+        /// <returns></returns>
+        [HttpPost("create-job")]
+        public IActionResult CreateJob(Job job)
+        {
+            _jobservice.CreateJob(job);
+            return Ok();
+        }
+
+        /// <summary>
+        /// 各従業員の業務データを取得するHTTPクエリ
+        /// </summary>
+        /// <param name="employee">従業員Model</param>
+        /// <returns></returns>
+        [HttpGet("read-all-employee-jobs/{employee}")]
+        public IActionResult ReadAllJobs(string employee)
+        {
+            var allJobs = _jobservice.ReadAllEmployeeJobs(employee);
+
+            return Ok(allJobs);
+        }
+
+        /// <summary>
+        /// 業務総合件数を取得するHTTPクエリ
+        /// </summary>
+        /// <returns>業務総合件数纏めたデータ</returns>
+        [HttpGet("read-total-jobs-employee")]
+        public IActionResult ReadTotalJobsOfEmployee()
+        {
+            var allJobs = _jobservice.ReadTotalJobsOfEmployee();
+
+            return Ok(allJobs);
+        }
+
+        /// <summary>
+        /// 対象業務データを更新するHTTPクエリ
+        /// </summary>
+        /// <param name="job">業務Model</param>
+        /// <returns></returns>
+        [HttpPut("update-job-by-id")]
+        public IActionResult UpdateJobById(Job job)
+        {
+            var updateJob = _jobservice.UpdateJobById(job);
+            return Ok(updateJob);
+        }
+
+        /// <summary>
+        /// 対象業務データを削除するHTTPクエリ
+        /// </summary>
+        /// <param name="id">対象業務ID</param>
+        /// <returns></returns>
+        [HttpDelete("delete-job-by-id/{id}")]
+        public IActionResult DeleteJobById(int id)
+        {
+            _jobservice.DeleteJobById(id);
+            return Ok();
+        }
+    }
+}
